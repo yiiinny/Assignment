075a84a80f864e601eee9572b210b78fd694f0fb
yiiinny <oyinkanzanny@gmail.com>
2025-03-10 11:57:45 +0100

    Add files via upload

---
+++ StackAssignment/StackAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+            namespace StackAssignment
+    {
+        class Program
+        {
+            static void Main()
+            {
+                var stack = new Stack();
+                stack.Push(1);
+                stack.Push(2);
+                stack.Push(3);
+
+                Console.WriteLine(stack.Pop()); // 3
+                Console.WriteLine(stack.Pop()); // 2
+                Console.WriteLine(stack.Pop()); // 1
+
+
+                //Console.WriteLine(stack.Pop()); This stimulates an exception.
+
+
+
+
+            }
+        }
+    }
+
+
+
+
+
---
+++ StackAssignment/StackAssignment/Stack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAssignment
+{
+   public class Stack
+    {
+         
+        private List<object> _items = new List<object>(); // Internal list to store stack items
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Null values cannot be pushed to the stack.");
+
+            _items.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_items.Count == 0)
+                throw new InvalidOperationException("Stack is empty. Cannot pop an element.");
+
+            int lastIndex = _items.Count - 1;
+            object topItem = _items[lastIndex];
+            _items.RemoveAt(lastIndex);
+            return topItem;
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+    }
+}
+
